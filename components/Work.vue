1320fcb00a29b0169e0d85a904c90b1b62b265b4
Javier Castillo <jfcastillov@gmail.com>
2020-05-17 15:09:03 +0200

    Update: posfly

--- components/Work.vue
+++ components/Work.vue
@@ -4,6 +4,7 @@
     <Nomada/>
     <Yalo/>
     <Adherente/>
+    <Posfly/>
   </section>
 </template>
 <script>
@@ -11,13 +12,15 @@ import RoyaleStudios from "~/components/RoyaleStudios.vue";
 import Nomada from "~/components/Nomada.vue";
 import Yalo from "~/components/Yalo.vue";
 import Adherente from "~/components/Adherente.vue";
+import Posfly from "~/components/Posfly.vue";
 
 export default {
   components: {
     RoyaleStudios,
     Nomada,
     Yalo,
-    Adherente
+    Adherente,
+    Posfly
   }
 };
 </script>
