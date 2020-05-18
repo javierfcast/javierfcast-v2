a17e7e795e249311c3ba6956a7d1462ef1272749
Javier Castillo <jfcastillov@gmail.com>
2020-05-18 21:14:41 +0200

    New section: Archive

---
+++ components/Archive.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="archive">
+    <a class="project-title" href="#">
+      <h4>Alessa.jewelry</h4>
+      <span class="project-type">E-Commerce</span>
+      <span>2020</span>
+    </a>
+    <a class="project-title" href="#">
+      <h4>Universidad Rafael Landívar .edu.gt</h4>
+      <span class="project-type">Website</span>
+    </a>
+    <a class="project-title" href="#">
+      <h4>Dev URL</h4>
+      <span class="project-type">Website</span>
+      <span>2020</span>
+    </a>
+    <a class="project-title" href="#">
+      <h4>Yalo Chat.com</h4>
+      <span class="project-type">Website</span>
+      <span>2020</span>
+    </a>
+    <a class="project-title" href="#">
+      <h4>Saul E. Mendez.com</h4>
+      <span class="project-type">Website</span>
+    </a>
+    <a class="project-title" href="#">
+      <h4>Datamecanic</h4>
+      <span class="project-type">Webapp</span>
+    </a>
+  </div>
+</template>
