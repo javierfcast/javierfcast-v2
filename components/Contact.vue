a9d5613444c413f32636a18684533ca98a098d4b
Javier Castillo <jfcastillov@gmail.com>
2020-05-24 10:11:41 +0200

    Add: footer with contact information

---
+++ components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="contact">
+    <h2 class="jf-h1">Get in touch</h2>
+    <h3>javierfcast@gmail.com</h3>
+    <div class="jf-spacer"></div>
+    <span class="jf-label">About this website</span>
+    <p>This is a static website built in Vue, Nuxt, and is being continuously deployed and hosted on Vercel.<br>
+    All the code is open source and you can check it out on Github.</p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "./assets/_mixins.scss";
+
+.contact{
+  @include container;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
     <Hero/>
     <About/>
     <Work/>
+    <Contact/>
   </section>
 </template>
 <script>
 import Hero from "~/components/Hero.vue";
 import About from "~/components/About.vue";
 import Work from "~/components/Work.vue";
+import Contact from "~/components/Contact.vue";
 
 export default {
   components: {
     Hero,
     About,
-    Work
+    Work,
+    Contact
   }
 };
 </script>
