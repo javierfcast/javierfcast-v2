ad32db0114485aca8c4af20ab36720207bbfca18
Javier Castillo <jfcastillov@gmail.com>
2020-05-24 20:42:16 +0200

    Organize components

--- components/Work.vue
+++ components/Work.vue
@@ -9,12 +9,12 @@
   </section>
 </template>
 <script>
-import RoyaleStudios from "~/components/RoyaleStudios.vue";
-import Nomada from "~/components/Nomada.vue";
-import Yalo from "~/components/Yalo.vue";
-import Adherente from "~/components/Adherente.vue";
-import Posfly from "~/components/Posfly.vue";
-import Archive from "~/components/Archive.vue";
+import RoyaleStudios from "~/components/projects/RoyaleStudios.vue";
+import Nomada from "~/components/projects/Nomada.vue";
+import Yalo from "~/components/projects/Yalo.vue";
+import Adherente from "~/components/projects/Adherente.vue";
+import Posfly from "~/components/projects/Posfly.vue";
+import Archive from "~/components/projects/Archive.vue";
 
 export default {
   components: {
