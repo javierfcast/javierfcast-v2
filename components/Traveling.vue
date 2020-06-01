66b16eb165e522831f77294e29e0caf3c278c6c3
Javier Castillo <jfcastillov@gmail.com>
2020-06-01 10:34:44 +0200

    Move Traveling component location

--- components/Traveling.vue
+++ components/Traveling.vue
@@ -19,21 +19,10 @@
         <li><a href="#">Chefchauen</a></li>
         <li><a href="#">Tanger</a></li>
 
-        <li><a href="#">Cancún</a></li>
-        <li><a href="#">Playa del Carmen</a></li>
-        <li><a href="#">Tulum</a></li>
-        <li><a href="#">Mexico City</a></li>
-        <li><a href="#">Isla de Flores</a></li>
-
-        <li><a href="#">San Francisco</a></li>
-        <li><a href="#">Los Angeles</a></li>
-        <li><a href="#">Las Vegas</a></li>
-        <li><a href="#">New York</a></li>
-        <li><a href="#">Miami</a></li>
-
         <li><a href="#">Moscow</a></li>
         <li><a href="#">St. Petersburg</a></li>
 
+        <li><a href="#">Milan</a></li>
         <li><a href="#">Venecia</a></li>
         <li><a href="#">Roma</a></li>
         <li><a href="#">Florencia</a></li>
@@ -42,6 +31,18 @@
 
         <li><a href="#">Santorini</a></li>
         <li><a href="#">Amsterdam</a></li>
+
+        <li><a href="#">San Francisco</a></li>
+        <li><a href="#">Los Angeles</a></li>
+        <li><a href="#">Las Vegas</a></li>
+        <li><a href="#">New York</a></li>
+        <li><a href="#">Miami</a></li>
+
+        <li><a href="#">Mexico City</a></li>
+        <li><a href="#">Cancún</a></li>
+        <li><a href="#">Playa del Carmen</a></li>
+        <li><a href="#">Tulum</a></li>
+        <li><a href="#">Bacalar</a></li>
 
         <li><a href="#">Bogota</a></li>
         <li><a href="#">Cartagena</a></li>
@@ -55,6 +56,15 @@
 
         <li><a href="#">Roatan</a></li>
         <li><a href="#">Cayo San Pedro</a></li>
+
+        <li><a href="#">Guatemala City</a></li>
+        <li><a href="#">Atitlán</a></li>
+        <li><a href="#">Antigua</a></li>
+        <li><a href="#">El Paredón</a></li>
+        <li><a href="#">Semuc Champey</a></li>
+        <li><a href="#">Izabal</a></li>
+        <li><a href="#">Flores</a></li>
+        <li><a href="#">Tikal</a></li>
 
       </ol>
     </div>
