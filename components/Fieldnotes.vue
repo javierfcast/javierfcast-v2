ee104f4d7ac8c4a7c38db1f645e2b39474879f93
Javier Castillo <jfcastillov@gmail.com>
2020-06-01 10:34:33 +0200

    Add: Fieldnotes

---
+++ components/Fieldnotes.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="fieldnotes" id="fieldnotes">
+    <div class="jf-container">
+      <h2 class="jf-h1">Fieldnotes</h2>
+      <h4 class="jf-lead">Here I'm sharing my personal design and development notes, reminders, lists of references, tools, resources, that I've found along the way.</h4>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "./assets/_mixins.scss";
+
+.fieldnotes{
+  background: #262524;
+  color: #ffffff;
+  a{
+    color: #ffffff;
+    border: none;
+  }
+}
+
+</style>
